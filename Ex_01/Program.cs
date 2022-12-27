3a8bc950ba16a0457aca864000dd24277d863af0
OLGA DRACH <dolchik88@mail.ru>
2022-12-27 17:43:59 +0300

    Сумма положительных и отрицательных элементов массива со случайными числами

---
+++ Ex_01/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 31: Задайте массив из 12 элементов, 
+// заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+
+const int SIZE = 5;
+const int LEFT_RANGE = -9;
+const int RIGHT_RANGE = 9;
+
+int[] array = new int[SIZE];
+//int sumPositive = 0;
+//int sumNegative = 0;
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+    }
+    return arr;
+}
+
+void SumPositiveAndNegative(int[] arr, out int sumP, out int sumN)
+{
+    sumP = 0;
+    sumN = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        {
+            sumP+=arr[i];
+        }
+        else
+        {
+            sumN+=arr[i];
+        }
+}
+}
+
+array = FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+SumPositiveAndNegative(array, out int sumP, out int sumN);
+
+Console.WriteLine($"Сумма положительных элементов = {sumP}, Сумма отрицательных элементов = {sumN}");
